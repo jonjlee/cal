9f0f0389898af4c05026c86a45ae6e9f3fd949e3
Jonathan Lee <jonjlee@gmail.com>
2024-10-11 00:07:21 -0700

    Update

---
+++ docs/assets/browser-mZzIvNvZ.js
@@ -0,0 +1,1 @@
+import{K as e}from"./db-Bqww2iDH.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
